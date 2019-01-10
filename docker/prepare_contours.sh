da03776517e0d5c7af9a2cce98238a1e28e954b1
Martin Ždila <m.zdila@gmail.com>
2019-01-10 17:42:46 +0100

    removed unrelated files

--- docker/prepare_contours.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-apt-get update \
-  && apt-get -y install --no-install-recommends postgresql-client \
-  && wget -qO- sandbox.freemap.sk/martin/contours.sql.gz | gzip -d | PGPASSWORD=secret psql -h freemap-postgis postgres postgres
--- docker/prepare_hillshading.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-mkdir hgt && wget -qO- sandbox.freemap.sk/martin/hillshade_warped.tif > hgt/hillshade_warped.tif
